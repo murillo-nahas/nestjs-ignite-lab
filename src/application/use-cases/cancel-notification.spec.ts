4b08f57cf47e8ee251b1b848a7fb90c3034689ea
murillo.nahas <murillonahas@hotmail.com>
2022-12-15 15:18:54 -0300

    feat: useCases

---
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,36 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { CancelNotification } from './cancel-notification';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { Notification } from '@application/entities/notification';
+import { Content } from '@application/entities/content';
+
+describe('Cancel notification', () => {
+  it('should be able to cancel a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    const notification = makeNotification();
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a notification when it does not exist', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
