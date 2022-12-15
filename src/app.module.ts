d2f93324848fabfe822dcf7eb9472bc212351264
murillo.nahas <murillonahas@hotmail.com>
2022-12-14 21:04:02 -0300

    feat: modules/services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [PrismaService],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
