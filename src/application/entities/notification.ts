d04280ba9626d05f472f11f16b6f860f0e58d7ab
murillo.nahas <murillonahas@hotmail.com>
2022-12-15 15:19:46 -0300

    feat: prisma, error handling and another stuff

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,11 +1,13 @@
-import { randomUUID } from 'crypto';
-import { Replace } from 'src/helpers/Replace';
+import { randomUUID } from 'node:crypto';
+import { Replace } from '../../helpers/Replace';
 import { Content } from './content';
+
 export interface NotificationProps {
   recipientId: string;
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
@@ -13,15 +15,18 @@ export class Notification {
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
   }
 
-  public get id(): string {
+  public get id() {
     return this._id;
   }
 
@@ -49,12 +54,24 @@ export class Notification {
     return this.props.category;
   }
 
-  public set readAt(readAt: Date | null | undefined) {
-    this.props.readAt = readAt;
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
   }
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get canceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
   }
 
   public get createdAt(): Date {
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notifications';
+import { CancelNotification } from './../../application/use-cases/cancel-notification';
 import { DatabaseModule } from './../database/database.module';
 import { SendNotification } from './../../application/use-cases/send-notification';
 import { NotificationsController } from './controllers/notifications.controller';
 import { Module } from '@nestjs/common';
+import { GetRecipientNotification } from '@application/use-cases/get-recipient-notifications';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
